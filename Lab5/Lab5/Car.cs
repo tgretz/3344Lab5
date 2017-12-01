7d989fa06c38208d6c10bfa7a7cdf3d8ae048933
Harley Baum <harleygwbaum@gmail.com>
2017-12-01 18:14:55 -0500

    Created Car class, Added Two Web Methods

---
+++ Lab5/Lab5/Car.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab5
+{
+    public class Car
+    {
+        public string VIN { get; set; }
+        public string make { get; set; }
+        public string model { get; set; }
+        public string year { get; set; }
+        public string type { get; set; }
+        public Decimal pricePerDay { get; set; }
+        public Boolean availability { get; set; }
+        public string picture { get; set; }
+
+
+
+    }
+}
--- Lab5/Lab5/CarService.asmx.cs
+++ Lab5/Lab5/CarService.asmx.cs
@@ -42,5 +42,53 @@ namespace Lab5
 
             return tv;
         }
+
+        //Web Method to store a Car obj in the database
+        //Accepts a Car Object using JSON
+        //Returns true if the Insert worked correctly and false if it did not
+        [WebMethod]
+        public Boolean StoreCar(Car carObj)
+        {
+            DBConnect objDB = new DBConnect();
+            string strSQL = "INSERT INTO Car (VIN, Make, Model, Year, Type, PricePerDay, Availability, Picture) " +
+                            "VALUES ('" + carObj.VIN + "', '" + carObj.make + "', '" + carObj.model + "', '" + carObj.year + "', '" + carObj.type + "', '" + carObj.pricePerDay + "', '" + carObj.availability + "', '" + carObj.picture + "')";
+
+            //returns number of records affected by the INSERT statement//returns -1 if there was an error
+            int result = objDB.DoUpdate(strSQL);
+
+            if (result > 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        //Web Method to get all info for a Car by passing in its VIN
+        //returns a Car Obj that contains all the information returned from the Query
+        [WebMethod]
+        public Car GetCarByVIN(string VIN)
+        {
+            DBConnect objDB = new DBConnect();
+            Car carObj = new Car();
+            string strSQL = "SELECT * FROM Car WHERE VIN = '" + VIN + "'";
+            int recordCount = 0;
+
+            objDB.GetDataSet(strSQL, out recordCount);
+
+            if (recordCount > 0)
+            {
+                carObj.VIN = objDB.GetField("VIN", 0).ToString();
+                carObj.make = objDB.GetField("Make", 0).ToString();
+                carObj.model = objDB.GetField("Model", 0).ToString();
+                carObj.year = objDB.GetField("Year", 0).ToString();
+                carObj.type = objDB.GetField("Type", 0).ToString();
+                carObj.pricePerDay = Convert.ToDecimal(objDB.GetField("PricePerDay", 0));
+                carObj.availability = Convert.ToBoolean(objDB.GetField("Availability", 0).ToString());
+                carObj.picture = objDB.GetField("Picture", 0).ToString();
+            }
+
+            return carObj;
+        }
     }
 }
